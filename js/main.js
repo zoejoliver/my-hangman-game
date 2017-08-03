78a16d06e06602abddbeb910821a8e8e76f1597e
Zoe Oliver <zoe@daleoliver.co.uk>
2017-08-03 16:18:19 +0100

    added buttons for all letters

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-body{
-text-align=center;
+
+var letterButton = '';
+var letter;
+
+//need function to take button press (letter) as input to check match with guessWord
+//from a(65) to z(90)
+for (var i=65; 90 >= i; i++){
+//gets letters from character codes
+  var letter = String.fromCharCode(i);
+//each letter in order on button
+  letterButton += '<button>' + letter + '</button>';
 }
+//
+document.getElementById('box').innerHTML = letterButton;
